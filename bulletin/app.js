967474d2ec91e61436d0ca51736ef08a6f8390a6
vanessa Michelle Valoy Vargas <vanessavaloy04@gmail.com>
2018-06-22 10:22:15 -0400

    first commit

---
+++ bulletin/app.js
@@ -0,0 +1,28 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', function(req, res) {
+  res.render('index', { message: 'TTP' });
+});
+
+
+
+
+
+module.exports = app;
